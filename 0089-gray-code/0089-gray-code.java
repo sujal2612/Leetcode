503deb0df01df6792bec70c28b1b97ebe9a5c00d
sujal2612 <aggarwalsujal2612@gmail.com>
2026-07-09 23:36:44 +0530

    Time: 5 ms (59.8%), Space: 53.6 MB (56.84%) - LeetHub

---
+++ 0089-gray-code/0089-gray-code.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> result = new ArrayList<>();
+        
+        int totalNumbers = 1 << n;
+        
+        for (int i = 0; i < totalNumbers; i++) {
+            result.add(i ^ (i >> 1));
+        }
+        
+        return result;
+    }
+}
