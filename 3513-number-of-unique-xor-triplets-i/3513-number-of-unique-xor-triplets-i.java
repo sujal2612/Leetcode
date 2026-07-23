9f0fe0531b020a53d55f987e7d2bc8377ed4d93c
sujal2612 <aggarwalsujal2612@gmail.com>
2026-07-24 00:46:47 +0530

    Time: 0 ms (100%), Space: 125.6 MB (10.2%) - LeetHub

---
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int uniqueXorTriplets(int[] nums) {
+        int n = nums.length;
+        
+        if (n < 3) {
+            return n;
+        }
+        int msb = (int) (Math.log(n) / Math.log(2));
+        return 1 << (msb + 1);
+    }
+}
