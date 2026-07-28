1913267f97baca184d79529a7f29267b20756d57
sujal2612 <aggarwalsujal2612@gmail.com>
2026-07-28 23:31:38 +0530

    Time: 32 ms (53.82%), Space: 48.1 MB (75.9%) - LeetHub

---
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public String smallestPalindrome(String s) {
+        int n = s.length();
+        int mid = n / 2;
+        char[] chars = s.toCharArray();
+        Arrays.sort(chars, 0, mid);
+        for (int i = 0; i < mid; i++) {
+            chars[n - 1 - i] = chars[i];
+        }
+        return new String(chars);
+    }
+}
